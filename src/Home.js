a95f1b69f21d983176919cf87e75b6e175ab6afa
FolusoSAMUEL <folusosamuel@gmail.com>
2023-03-31 14:59:14 +0100

    Even Listener

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+
+    const handleClick = () => {
+        console.log("hello, ninjas");
+    }
+
+const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+}
+
+    return (  
+        <div className="home">
+            <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick = {() => handleClickAgain('mario')}>Click me again</button>
+        </div>
+    );
+}
+ 
+export default Home;
