9fdacad73efc394786bbbdd31f3b66a9eadca05f
FolusoSAMUEL <folusosamuel@gmail.com>
2023-04-07 17:34:41 +0100

    fetch error

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,36 @@ import Bloglist from "./Bloglist";
 const Home = () => {
    const [blogs, setBlogs] = useState(null);
    const [isPending, setIsPending] = useState(true);
+const [error, setError] = useState(null);
 
    const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
    }
 
-
 useEffect(() => {
     setTimeout(() => {
-fetch('http://localhost:8000/blogs').then(res => {
+fetch('http://localhost:8000/blogss').then(res => {
+    if(!res.ok){
+        throw Error('could not fetch the data for that resource');
+    }
     return res.json();
-})
-.then(data => {
-    setBlogs(data);
-    setIsPending(false); 
-})
-}, 1000);
-   }, []);
+    })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false); 
+        })
+        .catch((err => {
+            setIsPending(false);
+            setError(err.message);
+        }))
+    }, 1000);
+    }, []);
 
 
     return (  
         <div className="home">
+        {error && <div>{error}</div>} 
         {isPending && <div>Loading...</div>}
        { blogs && <Bloglist blogs={blogs} title={'Blogs!'} handleDelete={handleDelete}/>}
         </div> 
